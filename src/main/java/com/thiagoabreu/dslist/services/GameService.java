78157048a0f18938ce3690c6f500b957a8cc4127
Thiago Abreu <tabreu.br@gmail.com>
2025-05-23 17:08:15 -0300

    SQL, projection, GET games by list

--- src/main/java/com/thiagoabreu/dslist/services/GameService.java
+++ src/main/java/com/thiagoabreu/dslist/services/GameService.java
@@ -4,6 +4,8 @@ package com.thiagoabreu.dslist.services;
 import com.thiagoabreu.dslist.dto.GameDTO;
 import com.thiagoabreu.dslist.dto.GameMinDTO;
 import com.thiagoabreu.dslist.entities.Game;
+import com.thiagoabreu.dslist.projections.GameMinProjection;
+import com.thiagoabreu.dslist.repositories.GameListRepository;
 import com.thiagoabreu.dslist.services.exception.ObjectNotFoundException;
 import com.thiagoabreu.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,9 @@ public class GameService {
     @Autowired
     public GameRepository gameRepository;
 
+    @Autowired
+    public GameListRepository gameListRepository;
+
     @Transactional(readOnly = true)
     public GameDTO findById(Long id) {
         return gameRepository.findById(id)
@@ -25,10 +30,19 @@ public class GameService {
                 .orElseThrow(() -> new ObjectNotFoundException("Game not found with id: " + id));
     }
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(GameMinDTO::new).toList();
     }
 
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        if (!gameListRepository.existsById(listId)) {
+            throw new ObjectNotFoundException("List not found with id: " + listId);
+        }
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(GameMinDTO::new).toList();
+    }
 
 }
